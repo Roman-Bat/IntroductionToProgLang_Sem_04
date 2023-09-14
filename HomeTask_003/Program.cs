8b7d1c7fa2abf952f47549a4a11dbd10beb4453c
Roman_Batrakov <kyct69@yandex.ru>
2023-09-15 00:17:29 +0300

    Выполнил третье домашнее задание

--- HomeTask_003/Program.cs
+++ HomeTask_003/Program.cs
@@ -2,3 +2,19 @@
 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 6, 1, 33 -> [6, 1, 33]
 */
+int [] randMass = new int[8];
+Random rand = new Random();
+
+string main(int N)
+{
+for (int i = 0; i <= N; i ++)
+{
+    randMass[i] = rand.Next(0, 20);
+    if (i < N) Console.Write(randMass[i] + ", ");
+    else Console.Write(randMass[i]);
+}
+    return "";
+}
+Console.Write("Элементы массива [");
+Console.Write(main(7));
+Console.Write("]");
