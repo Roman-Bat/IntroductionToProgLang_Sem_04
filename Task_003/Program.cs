a87d6ef411d17283919c06ceb66bb8a663b763fc
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 22:25:58 +0300

    Выполнил третье задание

--- Task_003/Program.cs
+++ Task_003/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 28:
+Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+4 --> 24
+5 --> 120
+*/
+int MessageUser (string message)
+{
+    Console.WriteLine(message);
+    int N = Convert.ToInt32(Console.ReadLine());
+    return N;
+}
+int Proizved (int N)
+{
+    int proizved = 1;
+    for (int i = 1; i <= N; i ++)
+    {
+        proizved = proizved * i;
+    }
+    return proizved;
+}
+
+int N = MessageUser("Введите число N");
+int proizved = Proizved(N);
+Console.WriteLine("Произведение всех чисел от 1 до " + N + " = " + proizved);
