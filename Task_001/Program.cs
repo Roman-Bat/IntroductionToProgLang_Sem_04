c2ca8039a54997257370e8037295ea3fb712e25f
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 22:03:43 +0300

    Добавил задания и решил первое задание

---
+++ Task_001/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+*/
+int MessageUser (string message)
+{
+    Console.WriteLine(message);
+    int A = Convert.ToInt32(Console.ReadLine());
+    return A;
+}
+int Summa (int A)
+{
+    int summa = 0;
+    for (int i = 1; i <= A; i ++)
+    {
+        summa = summa + i;
+    }
+    return summa;
+}
+
+int A = MessageUser("Введите число А");
+int summa = Summa(A);
+Console.WriteLine("Сумма всех чисел от 1 до " + A + " = " + summa);
