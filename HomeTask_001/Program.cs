d5df7d930fd1364aeea05855ad953015461f45c1
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 23:38:08 +0300

    Выполнил первое домашнее задание

--- HomeTask_001/Program.cs
+++ HomeTask_001/Program.cs
@@ -3,3 +3,25 @@
 3, 5 -> 243 (3⁵)
 2, 4 -> 16
 */
+int getNumber (string message)
+{
+    Console.Write(message);
+    int getNumber = Convert.ToInt32(Console.ReadLine());
+    return getNumber;
+}
+
+long exponentiation (int A, int B)
+{
+    long expo = 1;
+    long uA = Convert.ToInt64(A);
+    for (int i = 0; i < B; i++)
+    {
+        expo *= uA;
+    }
+    return expo;
+     
+}
+
+int A = getNumber("Введите число А:");
+int B = getNumber("Введите степень B:");
+Console.WriteLine(exponentiation(A, B));
