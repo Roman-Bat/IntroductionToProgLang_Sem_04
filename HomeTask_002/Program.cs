742956ecc90efb8c62ec1c22f74bdffc460d8f9f
Roman_Batrakov <kyct69@yandex.ru>
2023-09-14 23:55:43 +0300

    Выполнил второе домашнее задание

--- HomeTask_002/Program.cs
+++ HomeTask_002/Program.cs
@@ -4,3 +4,25 @@
 82 -> 10
 9012 -> 12
 */
+// Получаем от пользователя число
+int getNumber (string message)
+{
+    Console.Write(message);
+    int getNumber = Convert.ToInt32(Console.ReadLine());
+    return getNumber;
+}
+// Разделяем число на разряды и суммируем каждый разряд как число
+int DigitSum (int number)
+{
+    int Digit = 0;
+    while (number != 0)
+    {
+        Digit += number % 10;
+        number /= 10; 
+    }
+    return Digit;
+}
+// тело программы
+int number = getNumber("Введите число :");
+Console.Write("Сумма всех разрядов числа равна: ");
+Console.WriteLine(DigitSum(number));
